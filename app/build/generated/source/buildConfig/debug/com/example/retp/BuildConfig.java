c668b46db38c3c0b3756140cd901eb19d6c59999
Son <danghongson0807@gmail.com>
2021-05-17 14:53:28 +0700

    update google map api

--- app/build/generated/source/buildConfig/debug/com/example/retp/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/retp/BuildConfig.java
@@ -9,4 +9,6 @@ public final class BuildConfig {
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
+  // Field from the variant API
+  public static final String MAPS_API_KEY = "AIzaSyAZXOXLJGbH3GRH4SITgkFhTitUCpxaPDE";
 }
