b93e3af8209855bd9b19d3d86bb4099e00458dba
unknown <thinhtran0206@gmail.com>
2021-05-22 20:58:43 +0700

    sub chat

---
+++ app/src/main/java/com/example/retp/subchat_activity2.java
@@ -0,0 +1,14 @@
+package com.example.retp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class subchat_activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subchat_activity2);
+    }
+}
