f343d1351a3351d011e9350ba8061ecf0cd650bb
unknown <thinhtran0206@gmail.com>
2021-05-24 15:09:54 +0700

    update lai sub chat activity

--- app/src/main/java/com/example/retp/subchat_activity2.java
+++ app/src/main/java/com/example/retp/subchat_activity2.java
@@ -3,12 +3,16 @@ package com.example.retp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class subchat_activity2 extends AppCompatActivity {
-
+    TextView message_user,message_time,message_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subchat_activity2);
+        message_user=findViewById(R.id.message_user);
+        message_time=findViewById(R.id.message_time);
+        message_text=findViewById(R.id.message_text);
     }
 }
